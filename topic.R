bb68d63e4b8d6d1eebe625ddfbfc23d5e4739640
DiegoBaldassar <diego@baldassar.net>
2018-06-22 18:56:27 +0200

    Data cleaning corrections

--- topic.R
+++ topic.R
@@ -3,36 +3,40 @@ library(readr)
 library(tm)
 library(tidyverse)
 library(tidytext)
+
 set.seed(123)
 data<-read_csv("Texas Last Statement - CSV.csv")
+good_indexes <- data$LastStatement != "None"
 
 # column TDCJNumber can be used as indentifier for each observation
 # column LastStatement has the text of the last statement
 
 # there are some problems with some characters, let's convert to ascii
-data$LastStatement<-iconv(data$LastStatement,from="UTF-8",to="ASCII//TRANSLIT")
+data$LastStatement <- iconv(data$LastStatement, from = "UTF-8", to = "ASCII//TRANSLIT")
 
 # get document term matrix
-docs <- data.frame(doc_id = data$TDCJNumber,
-                   text = data$LastStatement,
+docs <- data.frame(doc_id = data$TDCJNumber[good_indexes],
+                   text = data$LastStatement[good_indexes],
                    stringsAsFactors = FALSE)
-ds <- DataframeSource(docs)
-x <- Corpus(ds)
-x <- tm_map(x, content_transformer(tolower))
-x <- tm_map(x, removePunctuation)
-x <- tm_map(x, removeWords, stopwords("english"))
+ds  <- DataframeSource(docs)
+x   <- Corpus(ds)
+x   <- tm_map(x, content_transformer(tolower))
+x   <- tm_map(x, removePunctuation)
+x   <- tm_map(x, removeWords, stopwords("english"))
 dtm <- DocumentTermMatrix(x)
 
 # get document term matrix (another way)
-tokens<-unnest_tokens(data,word, LastStatement,token="ngrams",n=1)
-words<-tokens %>% anti_join(stop_words) %>% group_by(TDCJNumber,word) %>% summarise(count=n())
-
-dtm2<-words %>%   cast_dtm(TDCJNumber, word, count)
+tokens <- unnest_tokens(data,word, LastStatement,token="ngrams",n=1)
+words  <- tokens %>% anti_join(stop_words) %>% group_by(TDCJNumber,word) %>% summarise(count=n())
+# Possible elaborations here...
+dtm2   <- words  %>% cast_dtm(TDCJNumber, word, count)
 
 
 
 rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-dtm.new   <- dtm[rowTotals> 0, ]           #remove all docs without words
+# dtm.new   <- dtm[rowTotals> 0, ]           #remove all docs without words
+dtm.new <- dtm
+print(which(rowTotals <= 0)) # no null rowTotals...
 
 
 ap_lda <- LDA(dtm.new, k = 5, control = list(seed = 1234))
@@ -66,14 +70,14 @@ beta_spread %>% top_n(20,abs(log_ratio)) %>% mutate(term = reorder(term, log_rat
   ggplot(aes(term,log_ratio)) + 
   geom_col()+
   coord_flip()+
-  labs(title="parole più diverse tra i due topic")
+  labs(title="parole pi? diverse tra i due topic")
 
 ap_documents <- tidy(ap_lda, matrix = "gamma")
 ap_documents
 
 ap_documents %>% ggplot(aes(x=factor(topic),y=gamma))+
   geom_boxplot()+
-  labs(title="probabilità delle parole dei vari topic")
+  labs(title="probabilit? delle parole dei vari topic")
 
 document_classification <- ap_documents %>%
   group_by(document) %>%
