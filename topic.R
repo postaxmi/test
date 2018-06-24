52c56c6936d814f524524495cda39cf90f7639bf
postaxmi <postaxmi@users.noreply.github.com>
2018-06-24 18:15:34 +0200

    improvements, consider stemming and tfidf

--- topic.R
+++ topic.R
@@ -3,6 +3,7 @@ library(readr)
 library(tm)
 library(tidyverse)
 library(tidytext)
+library(slam)
 
 set.seed(123)
 data<-read_csv("Texas Last Statement - CSV.csv")
@@ -18,25 +19,37 @@ data$LastStatement <- iconv(data$LastStatement, from = "UTF-8", to = "ASCII//TRA
 docs <- data.frame(doc_id = data$TDCJNumber[good_indexes],
                    text = data$LastStatement[good_indexes],
                    stringsAsFactors = FALSE)
-ds  <- DataframeSource(docs)
-x   <- Corpus(ds)
-x   <- tm_map(x, content_transformer(tolower))
-x   <- tm_map(x, removePunctuation)
-x   <- tm_map(x, removeWords, stopwords("english"))
-dtm <- DocumentTermMatrix(x)
+
+### 
+ # function to get document term matrix
+ # documents: dataframe with columns doc_id and text
+ # tfidf_threshold: (optional, default 0) minimum tf-idf value for each word to be kept in the dtm vocabulary
+getDtm<-function(documents, tfidf_threshold=0){
+  ds  <- DataframeSource(docs)
+  x   <- Corpus(ds)
+  # remove stpwords, use stemming, remove punctuation and numbers, to lower
+  #x   <- tm_map(x, content_transformer(tolower))
+  #x   <- tm_map(x, removePunctuation)
+  #x   <- tm_map(x, removeWords, stopwords("english"))
+  dtm <- DocumentTermMatrix(x, control = list(stemming = TRUE, stopwords = TRUE, minWordLength = 3, removeNumbers = TRUE, removePunctuation = TRUE))
+
+  # reduce vocabulary of the dtm, consider only words with high values of tf-idf
+  term_tfidf<-tapply(dtm$v/row_sums(dtm)[dtm$i], dtm$j, mean) *log2(nDocs(dtm)/col_sums(dtm > 0))
+  print(paste("median tf-idf: ",median(term_tfidf)))
+
+  dtm_reduced <- dtm[, term_tfidf >= tfidf_threshold]
+  rowTotals <- apply(dtm_reduced , 1, sum) #Find the sum of words in each Document
+  dtm_reduced   <- dtm[rowTotals> 0, ]           #remove all docs without words
+  return(dtm_reduced)
+}
 
 # get document term matrix (another way)
 tokens <- unnest_tokens(data,word, LastStatement,token="ngrams",n=1)
-words  <- tokens %>% anti_join(stop_words) %>% group_by(TDCJNumber,word) %>% summarise(count=n())
+words  <- tokens %>% anti_join(stop_words,by="word") %>% group_by(TDCJNumber,word) %>% summarise(count=n())
 # Possible elaborations here...
 dtm2   <- words  %>% cast_dtm(TDCJNumber, word, count)
 
-
-
-rowTotals <- apply(dtm , 1, sum) #Find the sum of words in each Document
-# dtm.new   <- dtm[rowTotals> 0, ]           #remove all docs without words
-dtm.new <- dtm
-print(which(rowTotals <= 0)) # no null rowTotals...
+dtm.new<- getDtm(docs,tfidf_threshold = 0.1)
 
 
 ap_lda <- LDA(dtm.new, k = 5, control = list(seed = 1234))
